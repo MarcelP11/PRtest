e451c236cfe2a29e9b53ff764809581f91ca2ded
phar123 <pharach0@gmail.com>
2022-07-20 14:37:01 +0200

    Commit task1

--- src/PrintSquare.java
+++ src/PrintSquare.java
@@ -1,6 +1,6 @@
 public class PrintSquare {
     public static void main(String[] args) {
-        int size = 5;
+        int size = 10;
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
                 System.out.print("*");
